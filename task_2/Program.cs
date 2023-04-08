3aa31bafd718eaa75649e63580c1a2398ac01324
Lyas <Egorlyasotsky@yandex.ru>
2023-04-08 18:36:22 +0400

    2nd task is ready

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -3,3 +3,29 @@
 [3, 7, 23, 12] -> 19
 
 [-4, -6, 89, 6] -> 0 */
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+  int[] result = new int[size];
+  for (int i = 0; i < size; i++)
+  {
+    result[i] = new Random().Next(minValue, maxValue + 1);
+  }
+  return result;
+}
+
+int SearchSumElem(int[] array)
+{
+  int sum = 0;
+  for (int i = 0; i < array.Length; i += 2) // i++;
+  {
+    /* if (i % 2 == 0) */
+    sum += array[i];
+  }
+  return sum;
+}
+
+Console.Clear();
+int[] numbers = GetRandomArray(8, -10, 10);
+
+Console.WriteLine(String.Join(",", numbers));
+Console.WriteLine($"Сумма элементов на нечетных позициях равна: {SearchSumElem(numbers)}");
